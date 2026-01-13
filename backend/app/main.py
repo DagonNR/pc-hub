2de21cac11d4c2085e49ab4eb446c68afc04872c
Juan <juaanp567@gmail.com>
2026-01-12 19:10:44 -0600

    creacion de tabla client

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.routes.tickets import router as tickets_router
+from app.api.routes.clients import router as clients_router
 from app.core.database import Base, engine #Esto es momentaneo, en un futuro se usara Asemblic
 from app.models import ticket
 
@@ -9,6 +10,7 @@ app = FastAPI(title="PC Hub")
 def health():
     return {"message": "All is ok"}
 
+app.include_router(clients_router)
 app.include_router(tickets_router)
 
 @app.on_event("startup")
